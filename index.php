e228c76dabbf097ac1fe4903cf49835932248006
Robin Bantjes <originalmmd@gmail.com>
2018-02-06 17:00:58 +0200

    test crouch pdf

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ require __DIR__ . '/vendor/autoload.php';
 use mikehaertl\wkhtmlto\Pdf;
 echo __DIR__ ;
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
-$pdf = new Pdf('http://google.com');
+$pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/175');
 
 // On some systems you may have to set the path to the wkhtmltopdf executable
 // $pdf->binary = 'C:\...';
 
-if (!$pdf->saveAs('1page.pdf')) {
+if (!$pdf->saveAs('175.pdf')) {
     echo $pdf->getError();
 }
  ?>
