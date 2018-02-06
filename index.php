6fbc34c48ccb75dd5060f94c7016ef0483b9b332
Robin Bantjes <originalmmd@gmail.com>
2018-02-06 15:08:50 +0200

    firstDDD pdf try

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
 
 <?php
 require_once phpwkhtmltopdf/src/Pdf.php;
-
+echo "required";
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
 $pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/175');
-
+echo "got the url";
 // On some systems you may have to set the path to the wkhtmltopdf executable
 // $pdf->binary = 'C:\...';
 
 if (!$pdf->saveAs('page.pdf')) {
+  
     echo $pdf->getError();
 } ?>
