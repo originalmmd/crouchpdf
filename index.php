aae9077bc52e3c9f311ecb2a8eb06828a9e09442
Robin Bantjes <originalmmd@gmail.com>
2018-02-06 16:43:55 +0200

    with composer

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-echo "in the php tag";
-require_once /phpwkhtmltopdf/src/Pdf.php;
-echo "required";
+require __DIR__ . '/vendor/autoload.php';
+use mikehaertl\wkhtmlto\Pdf;
+
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
-$pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/175');
-echo "got the url";
+$pdf = new Pdf('http://google.com');
+
 // On some systems you may have to set the path to the wkhtmltopdf executable
 // $pdf->binary = 'C:\...';
 
 if (!$pdf->saveAs('page.pdf')) {
-
     echo $pdf->getError();
-} ?>
+}
+ ?>
