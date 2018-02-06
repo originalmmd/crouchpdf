20e044e60f7c447963f0c4528c1923cfb2465e56
Robin Bantjes <originalmmd@gmail.com>
2018-02-06 15:12:17 +0200

    fixing require

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "in the php tag";
-require_once phpwkhtmltopdf/src/Pdf.php;
+require_once /phpwkhtmltopdf/src/Pdf.php;
 echo "required";
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
 $pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/175');
