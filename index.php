31704b7f7a61118b55a8d9711387273841e7893f
Robin Bantjes <originalmmd@gmail.com>
2018-02-07 07:52:37 +0200

    with catalogues vars and email

--- index.php
+++ index.php
@@ -3,12 +3,23 @@ require __DIR__ . '/vendor/autoload.php';
 use mikehaertl\wkhtmlto\Pdf;
 echo __DIR__ ;
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
-$pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/175');
+$pdf = new Pdf('http://beta.crouchsales.co.za/catalogues/generate_catalogue/'.$_POST['catalogue_id']);
 
 // On some systems you may have to set the path to the wkhtmltopdf executable
 // $pdf->binary = 'C:\...';
 
-if (!$pdf->saveAs('175.pdf')) {
+if (!$pdf->saveAs($_POST['catalogue_name'].'.pdf')) {
     echo $pdf->getError();
+}else {
+  // the message
+  $msg = 'Your catalogue is ready!\nGet it here. <a href="http://108.61.211.236/crouchpdf/'.$_POST['catalogue_name']'.pdf">Download</a>';
+
+  // use wordwrap() if lines are longer than 70 characters
+  // $msg = wordwrap($msg,70);
+
+  // send email
+  mail("originalmmd@gmail.com","Testing the catalogue",$msg);
 }
+
+
  ?>
