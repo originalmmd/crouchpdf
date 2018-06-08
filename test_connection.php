592986ecea6c9d3afc65076e028c20896dad433e
Robin Bantjes <originalmmd@gmil.com>
2018-06-08 10:55:45 +0200

    test connection 5

--- test_connection.php
+++ test_connection.php
@@ -1,18 +1,28 @@
 <?php
-$link = mysql_connect('crouchsales.co.za', 'crouchsalesco_crouchbeta', '@Change.Score.50!');
-if (!$link) {
-    die('Not connected : ' . mysql_error());
-}
 
-// make foo the current db
-$db_selected = mysql_select_db('crouchsalesco_crouchbeta', $link);
-if (!$db_selected) {
-    die ('Can\'t use foo : ' . mysql_error());
-}
+// $link = mysql_connect('crouchsales.co.za', 'crouchsalesco_crouchbeta', '@Change.Score.50!');
+// if (!$link) {
+//     die('Not connected : ' . mysql_error());
+// }
+//
+// // make foo the current db
+// $db_selected = mysql_select_db('crouchsalesco_crouchbeta', $link);
+// if (!$db_selected) {
+//     die ('Can\'t use foo : ' . mysql_error());
+// }
 //
 // $result = mysql_query("SELECT * FROM email_reportings");
 // $results = mysql_fetch_assoc($result);
 // print_r($results);
 echo "working";
 
+$hostname='138.201.19.4';
+$username='crouchsalesco_crouchbeta';
+$password='@Change.Score.50!';
+$dbname='crouchsalesco_crouchbeta';
+$usertable='test';
+$yourfield = 'lat';
+
+mysql_connect($hostname,$username, $password) OR DIE ('Unable to connect to database! Please try again later.');
+mysql_select_db($dbname);
  ?>
