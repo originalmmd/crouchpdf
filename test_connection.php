d952c5743e2cf9871c78853abaf598144d79d603
Robin Bantjes <originalmmd@gmil.com>
2018-06-08 10:49:57 +0200

    test connection 4

--- test_connection.php
+++ test_connection.php
@@ -1,14 +1,14 @@
 <?php
-// $link = mysql_connect('crouchsales.co.za', 'crouchsalesco_crouchbeta', '@Change.Score.50!');
-// if (!$link) {
-//     die('Not connected : ' . mysql_error());
-// }
-//
-// // make foo the current db
-// $db_selected = mysql_select_db('crouchsalesco_crouchbeta', $link);
-// if (!$db_selected) {
-//     die ('Can\'t use foo : ' . mysql_error());
-// }
+$link = mysql_connect('crouchsales.co.za', 'crouchsalesco_crouchbeta', '@Change.Score.50!');
+if (!$link) {
+    die('Not connected : ' . mysql_error());
+}
+
+// make foo the current db
+$db_selected = mysql_select_db('crouchsalesco_crouchbeta', $link);
+if (!$db_selected) {
+    die ('Can\'t use foo : ' . mysql_error());
+}
 //
 // $result = mysql_query("SELECT * FROM email_reportings");
 // $results = mysql_fetch_assoc($result);
